c80df5fabfaa651983716424da556139f6262d6b
Louie Yang <louiey@naver.com>
2022-05-30 21:58:52 +0900

    timer state 작성, 상태 초기화

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_timer/screens/timer_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pomodo Timer App',
+      title: 'Pomodoro Timer App',
+      home: TimerScreen(),
     );
   }
 }
---
+++ screens/timer_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+enum TimerStatus { running, paused, stopped, resting }
+
+class TimerScreen extends StatefulWidget {
+  @override
+  _TimerScreenState createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends State<TimerScreen> {
+  static const WORK_SECONDS = 25;
+  static const REST_SECONDS = 5;
+
+  late TimerStatus _timerStatus;
+  late int _timer;
+  late int _pomodoroCount;
+
+  @override
+  void initState() {
+    super.initState();
+    _timerStatus = TimerStatus.stopped;
+    print(_timerStatus.toString());
+    _timer = WORK_SECONDS;
+    _pomodoroCount = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> _runningButtons = [
+      ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          1 == 2 ? '계속하기' : '일시정지',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        style: ElevatedButton.styleFrom(primary: Colors.blue),
+      ),
+      Padding(
+        padding: EdgeInsets.all(20),
+      ),
+      ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          '포기하기',
+          style: TextStyle(fontSize: 16),
+        ),
+        style: ElevatedButton.styleFrom(primary: Colors.grey),
+      ),
+    ];
+
+    final List<Widget> _stoppedButtons = [
+      ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          '시작하기',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: 1 == 2 ? Colors.green : Colors.blue,
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('뽀모로도 타이머'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.85,
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: Center(
+              child: Text(
+                '00:00',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: 1 == 2 ? Colors.green : Colors.blue,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: 1 == 2
+                ? const []
+                : 1 == 2
+                    ? _stoppedButtons
+                    : _runningButtons,
+          )
+        ],
+      ),
+    );
+  }
+}
